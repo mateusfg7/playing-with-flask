e49928bea2092158382f833f0080fb28c6029f60
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2024-04-16 17:41:55 +0000

    feat: add more example routes

--- app.py
+++ app.py
@@ -1,7 +1,24 @@
 from flask import Flask
+from markupsafe import escape
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello_word():
-    return "<h1>Hello World!</h1>"
+def index():
+    return "Index Page"
+
+@app.route('/hello')
+def hello_world():
+    return "Hello World"
+
+@app.route('/user/<username>')
+def show_user_profile(username: str):
+    return f'User {escape(username)}'
+
+@app.route('/post/<int:post_id>')
+def show_post(post_id: int):
+    return f'Post {escape(post_id)}'
+
+@app.route('/path/<path:subpath>')
+def show_subpath(subpath):
+    return f'Subpath {escape(subpath)}'
